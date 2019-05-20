b7931005454653af222a993a740732e2d70f508d
carcamcue <carcamcue@gmail.com>
2019-05-20 21:48:22 +0200

    Add typeit for writing effect.

--- components/HomeClaim.vue
+++ components/HomeClaim.vue
@@ -5,6 +5,7 @@
         <div class="col-md-12 col-sm-12 col-xs-12 text-center">
           <div class="claim wow fadeInLeft">
             <h2>{{ data.claim }}</h2>
+            <div id="type-header"></div>
           </div>
         </div>
       </div>
@@ -13,6 +14,7 @@
 </template>
 
 <script>
+import TypeIt from 'typeit'
 export default {
   data() {
     return {
@@ -20,6 +22,56 @@ export default {
         claim: 'Tecnología para la Transformación Social'
       }
     }
+  },
+  mounted() {
+    const myTypeItInstance = new TypeIt('#type-header')
+      .options({
+        speed: 100,
+        deleteSpeed: 100
+      })
+      .type('Vota las propuestas de tu ciudad')
+      .pause(1550)
+      .delete(6)
+      .type('pueblo')
+      .pause(1750)
+      .delete(6)
+      .type('cooperativa')
+      .pause(1750)
+      .options({ deleteSpeed: 75 })
+      .delete(100)
+      .pause(1300)
+      .type('Participa en las decisiones de tu ciudad')
+      .pause(1750)
+      .delete(6)
+      .type('<b>ciudad</b>')
+      .pause(1500)
+      .delete(100)
+      .pause(1000)
+      .type('Consulta los presupuestos de tu ayuntamiento')
+      .pause(1600)
+      .delete(15)
+      .type('TU AYUNTAMIENTO')
+      .pause(2000)
+      .options({ deleteSpeed: 50, speed: 75 })
+      .delete(100)
+      .pause(750)
+      .type('Lanza propuestas ciudadanas')
+      .pause(1300)
+      .type('<br><b>Tu ayuntamiento te necesita</b>')
+      .pause(1800)
+      .delete(200)
+      .pause(1750)
+      .go()
+
+    setTimeout(() => {
+      myTypeItInstance.reset()
+      myTypeItInstance.go()
+    }, 60000)
+
+    /*
+        'Participa en las decisiones de tu ciudad',
+        'Consulta los presupuestos de tu ayuntamiento',
+        'Lanza propuestas ciudadanas' */
   }
 }
 </script>
